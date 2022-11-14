88e7d8d42f6ea28a40f67836c618337d4277346b
Jose Arturo <arturotoll@gmail.com>
2022-11-14 11:45:09 -0500

    asdf

--- src/App.svelte
+++ src/App.svelte
@@ -12,14 +12,7 @@
           target: document.querySelector("#camera"), // Or '#yourElement' (optional)
         },
         decoder: {
-          readers: [
-            {
-              format: "ean_reader",
-              config: {
-                supplements: ["ean_5_reader", "ean_2_reader"],
-              },
-            },
-          ],
+          readers: ["ean_reader", "ean_8_reader"],
         },
       },
       function (err) {
@@ -31,47 +24,6 @@
         Quagga.start();
       }
     );
-    Quagga.onProcessed(function (result) {
-      var drawingCtx = Quagga.canvas.ctx.overlay,
-        drawingCanvas = Quagga.canvas.dom.overlay;
-
-      if (result) {
-        if (result.boxes) {
-          drawingCtx.clearRect(
-            0,
-            0,
-            parseInt(drawingCanvas.getAttribute("width")),
-            parseInt(drawingCanvas.getAttribute("height"))
-          );
-          result.boxes
-            .filter(function (box) {
-              return box !== result.box;
-            })
-            .forEach(function (box) {
-              Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
-                color: "green",
-                lineWidth: 2,
-              });
-            });
-        }
-
-        if (result.box) {
-          Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
-            color: "#00F",
-            lineWidth: 2,
-          });
-        }
-
-        if (result.codeResult && result.codeResult.code) {
-          Quagga.ImageDebug.drawPath(
-            result.line,
-            { x: "x", y: "y" },
-            drawingCtx,
-            { color: "red", lineWidth: 3 }
-          );
-        }
-      }
-    });
     Quagga.onDetected(function (data) {
       console.log("Es este", data.codeResult.code);
       code = data.codeResult.code;
@@ -81,6 +33,4 @@
 
 <div id="camera" />
 
-{#if code}
-  <h1>Codigo: {code}</h1>
-{/if}
+<h1>Codigo: {code}</h1>

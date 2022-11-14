0c2ab42dc1006abb0c709d1ff19af102c3007b6e
Jose Arturo <arturotoll@gmail.com>
2022-11-14 10:48:13 -0500

    add selects

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,62 @@
 <script>
   import { onMount } from "svelte";
   import Quagga from "quagga";
-  onMount(() => {
-    Quagga.init(
-      {
-        inputStream: {
-          name: "Live",
-          type: "LiveStream",
-          target: document.querySelector("#camera"), // Or '#yourElement' (optional)
+  const decodes = [
+    "code_128_reader",
+    "ean_reader",
+    "ean_8_reader",
+    "code_39_reader",
+    "code_39_vin_reader",
+    "codabar_reader",
+    "upc_reader",
+    "upc_e_reader",
+    "i2of5_reader",
+    "2of5_reader",
+    "code_93_reader",
+  ];
+  let code;
+  let decoder = "code_128_reader";
+  const changeDecoder = (decoder) => {
+    console.log("cada vez que el decoder cambie", decoder);
+    try {
+      Quagga.init(
+        {
+          inputStream: {
+            name: "Live",
+            type: "LiveStream",
+            target: document.querySelector("#camera"), // Or '#yourElement' (optional)
+          },
+          decoder: {
+            readers: [decoder],
+          },
         },
-        decoder: {
-          readers: ["code_128_reader"],
-        },
-      },
-      function (err) {
-        if (err) {
-          console.log(err);
-          return;
+        function (err) {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          console.log("Initialization finished. Ready to start");
+          Quagga.start();
         }
-        console.log("Initialization finished. Ready to start");
-        Quagga.start();
-      }
-    );
-
-    Quagga.onDetected(function (data) {
-      console.log("Es este", data.codeResult.code);
-      document.querySelector("#resultado").innerText = data.codeResult.code;
-    });
-  });
+      );
+      Quagga.onDetected(function (data) {
+        console.log("Es este", data.codeResult.code);
+        code = data.codeResult.code;
+      });
+    } catch {
+      console.log("no se pudo iniciar");
+    }
+  };
+  $: changeDecoder(decoder);
 </script>
 
+<select name="decoders" id="decoders" bind:value={decoder}>
+  {#each decodes as decoder}
+    <option value={decoder}>{decoder}</option>
+  {/each}
+</select>
 <div id="camera" />
-<p id="resultado" />
+
+{#if code}
+  <h1>Codigo: {code}</h1>
+{/if}

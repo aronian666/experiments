0a37968648b7beadef3a87fd12bd6e4e930ece0e
Jose Arturo <arturotoll@gmail.com>
2022-11-14 11:24:34 -0500

    ahora pe raton

--- src/App.svelte
+++ src/App.svelte
@@ -1,60 +1,84 @@
 <script>
   import { onMount } from "svelte";
   import Quagga from "quagga";
-  const decodes = [
-    "code_128_reader",
-    "ean_reader",
-    "ean_8_reader",
-    "code_39_reader",
-    "code_39_vin_reader",
-    "codabar_reader",
-    "upc_reader",
-    "upc_e_reader",
-    "i2of5_reader",
-    "2of5_reader",
-    "code_93_reader",
-  ];
   let code;
-  let decoder = "code_128_reader";
-  const changeDecoder = (decoder) => {
-    console.log("cada vez que el decoder cambie", decoder);
-    try {
-      Quagga.init(
-        {
-          inputStream: {
-            name: "Live",
-            type: "LiveStream",
-            target: document.querySelector("#camera"), // Or '#yourElement' (optional)
-          },
-          decoder: {
-            readers: [decoder],
-          },
+
+  onMount(() => {
+    Quagga.init(
+      {
+        inputStream: {
+          name: "Live",
+          type: "LiveStream",
+          target: document.querySelector("#camera"), // Or '#yourElement' (optional)
         },
-        function (err) {
-          if (err) {
-            console.log(err);
-            return;
-          }
-          console.log("Initialization finished. Ready to start");
-          Quagga.start();
+        decoder: {
+          readers: [
+            {
+              format: "ean_reader",
+              config: {
+                supplements: ["ean_5_reader", "ean_2_reader"],
+              },
+            },
+          ],
+        },
+      },
+      function (err) {
+        if (err) {
+          console.log(err);
+          return;
         }
-      );
-      Quagga.onDetected(function (data) {
-        console.log("Es este", data.codeResult.code);
-        code = data.codeResult.code;
-      });
-    } catch {
-      console.log("no se pudo iniciar");
-    }
-  };
-  $: changeDecoder(decoder);
+        console.log("Initialization finished. Ready to start");
+        Quagga.start();
+      }
+    );
+    Quagga.onProcessed(function (result) {
+      var drawingCtx = Quagga.canvas.ctx.overlay,
+        drawingCanvas = Quagga.canvas.dom.overlay;
+
+      if (result) {
+        if (result.boxes) {
+          drawingCtx.clearRect(
+            0,
+            0,
+            parseInt(drawingCanvas.getAttribute("width")),
+            parseInt(drawingCanvas.getAttribute("height"))
+          );
+          result.boxes
+            .filter(function (box) {
+              return box !== result.box;
+            })
+            .forEach(function (box) {
+              Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+                color: "green",
+                lineWidth: 2,
+              });
+            });
+        }
+
+        if (result.box) {
+          Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
+            color: "#00F",
+            lineWidth: 2,
+          });
+        }
+
+        if (result.codeResult && result.codeResult.code) {
+          Quagga.ImageDebug.drawPath(
+            result.line,
+            { x: "x", y: "y" },
+            drawingCtx,
+            { color: "red", lineWidth: 3 }
+          );
+        }
+      }
+    });
+    Quagga.onDetected(function (data) {
+      console.log("Es este", data.codeResult.code);
+      code = data.codeResult.code;
+    });
+  });
 </script>
 
-<select name="decoders" id="decoders" bind:value={decoder}>
-  {#each decodes as decoder}
-    <option value={decoder}>{decoder}</option>
-  {/each}
-</select>
 <div id="camera" />
 
 {#if code}
